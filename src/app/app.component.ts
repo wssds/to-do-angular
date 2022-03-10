668faf035c6565a7f4d3665c31d49ec302848ffa
wssds Sn Gk <ssean.gerigk@gmx.net>
2022-03-10 19:15:31 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tallence';
+  display=false;
+
+  @ViewChild('task') inputText: any;
+
+  list:any[]=[];
+
+
+  backgroundcolor="white";
+  
+  toggle(){
+    this.display=!this.display;
+  }
+
+  addTask(item:string){
+    if(item.length >= 1){
+      this.list.push({id:this.list.length,name:item})
+    } else {
+      alert('Deine Notiz muss mindestens 1 Zeichen enthalten');
+    }
+      
+    
+    console.log(this.list);
+
+    this.inputText.nativeElement.value='';
+
+  }
+
+  removeTask(id:number){
+    console.log(id);
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+
+  doneTask(){
+    this.backgroundcolor="darkgrey";
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatRadioModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
